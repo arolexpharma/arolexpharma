931ce0caa014a7c8c802f17227465d24c3c43a38
Aarifansarikht <arifansarikhtz@gmail.com>
2025-02-20 23:48:20 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,14 +3,13 @@ module.exports = {
   siteUrl: "https://www.arolexpharma.com",
   generateRobotsTxt: true,
   changefreq: "weekly",
-  priority: 1.0,
   generateIndexSitemap: true,
   transform: async (config, path) => {
     return {
       loc: path.replace("https://arolexpharma.com", "https://www.arolexpharma.com"),
       lastmod: new Date().toISOString(),
       changefreq: "weekly",
-      priority: 1.0,
+      priority: path === "/" ? 1.0 : 0.8,
     };
   }
 };
